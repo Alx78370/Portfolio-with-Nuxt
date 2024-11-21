b9e18e969b1daa418fa0d39472daf70d4ed1c78d
Alexis VACHET <alexis.vachet@outlook.fr>
2024-11-21 12:03:37 +0100

    fix toggleLang size

--- components/ToggleLang.vue
+++ components/ToggleLang.vue
@@ -10,24 +10,24 @@ function toggleLocale() {
 const flags = {
     en: {
         icon: "circle-flags:gb",
-        text: "English ",
+        text: "En ",
     },
     fr: {
         icon: "circle-flags:fr",
-        text: "Français",
+        text: "Fr",
     }
 };
 </script>
 
 <template>
     <button 
-        class="flex flex-col items-center gap-1 absolute md:fixed top-16 right-5 md:top-60 md:right-5 z-10" 
+        class="flex flex-col items-center gap-1 absolute md:fixed top-5 right-5 md:top-60 md:right-5 z-10" 
         @click="toggleLocale" 
         tabindex="0" 
         :aria-label="t('ariaLabel.toggleLang')">
         <Icon
             :name="locale === 'en' ? flags.en.icon || flags.en.text : flags.fr.icon || flags.fr.text"
-            class="w-10 h-10 hover:border-2 hover:border-[#00DC82] rounded-full"
+            class="w-6 h-6 md:w-10 md:h-10 hover:border-2 hover:border-[#00DC82] rounded-full"
         />
         <p class="text-xs text-white font-mono md:w-14">
             {{ locale === 'en' ? flags.en.text : flags.fr.text }}
